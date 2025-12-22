560dceebfc14f4e3636148bb09bc6a6cc4fb63de
Chelsea Sweet <chelsea.rae.sweet@gmail.com>
2025-12-22 15:22:11 -0500

    updated text on UI

--- rag_bot_streamlit.py
+++ rag_bot_streamlit.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from rag_pipeline import ask
 
-st.title("21 CFR Part 11 RAG Assistant")
+st.title("GxP Guide")
 
-question = st.text_input("Ask a question about Part 11:")
+question = st.text_input("Ask a question about GxP compliance (so far, I only know about 21 CFR Part 11 - I'm still learning!):")
 
-if st.button("Submit"):
+if st.button("Ask"):
     answer = ask(question)
     st.write(answer)
